ff0dcf36db6c214471c53ddf80d383ee3463efff
Lazar <lazarpetrovic93@gmail.com>
2024-08-28 12:29:48 +0200

    Added types.

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,30 @@ export interface Product {
     category: string;
     stock: number;
 }
+
+export interface ButtonProps {
+    onButtonClickFunc: () => void;
+    type?: 'delete' | 'edit';
+    text: string;
+    className: string;
+}
+
+
+export interface ModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    title?: string;
+    children: React.ReactNode;
+    footer: React.ReactNode;
+}
+
+export interface EditProductModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    product: Product | null;
+}
+export interface DeleteProductModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    product: Product | null;
+}
